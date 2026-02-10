1174c8a12e036522112950ba836d153486128d1a
CDR4733 <shsing2@naver.com>
2026-02-10 16:53:10 +0900

    [add] Joi 유효성 검증
    * Joi 유효성 검증

--- src/middlewares/error-handler.middleware.js
+++ src/middlewares/error-handler.middleware.js
@@ -1,6 +1,14 @@
 export const errorHandler = (err, req, res, next) => {
   console.error(err);
 
+  // Joi에게 err 잡힌 경우
+  if (err.name === "ValidationError") {
+    return res.status(400).json({
+      status: 400,
+      message: err.message, // Joi에서 지정한 메시지로 에러메시지 출력
+    });
+  }
+
   return res.status(500).json({
     status: 500,
     message: "예상치 못한 에러가 발생했습니다. 관리자에게 문의해 주세요.",
